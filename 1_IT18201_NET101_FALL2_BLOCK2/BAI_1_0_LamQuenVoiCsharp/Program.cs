fa54b343d177868397fdd394de7f7cf04d42fe1b
DUNGNA-LAPTOPAS\Dungna29_AsusR5 <dungna29@fpt.edu.vn>
2022-11-01 13:55:45 +0700

    1

---
+++ 1_IT18201_NET101_FALL2_BLOCK2/BAI_1_0_LamQuenVoiCsharp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI_1_0_LamQuenVoiCsharp
+{
+    internal class Program
+    {  /*
+         * Làm quen các phím tắt cơ bản
+         * 1. Để chạy chương trình Ctrl + F5 để chạy chương trình.
+         * 2. Ctrl + D: Nhân bản 1 hoặc nhiều dòng code
+         * 3. Ctrl + K + C: Comment đoạn code
+         * 4. Ctrl + K + U: Mở comment
+         * 5. Ctrl + space: Gợi ý code
+         * 6. Ctrl + K + D: Format Code
+         */
+        /*
+         * Comment trên 1 khối lệnh
+         */
+        static void Main(string[] args)
+        {
+            //Hiển thị được tiếng việt
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //cw + tab
+            Console.WriteLine("Chào các bạn đến với C#1");
+            Console.WriteLine("Chào các bạn đến với C#1");
+            Console.WriteLine("Chào các bạn đến với C#1");
+            Console.WriteLine("Chào các bạn đến với C#1");
+            Console.WriteLine("Chào các bạn đến với C#1");
+            Console.WriteLine("Chào các bạn đến với C#1");
+            Console.WriteLine("Chào các bạn đến với C#1");
+            Console.WriteLine("Chào các bạn đến với C#1");
+            Console.WriteLine("Chào các bạn đến với C#1");
+            Console.WriteLine("Chào các bạn đến với C#1");
+
+
+
+        }
+        /*
+         * 1. Code
+         * 2. Chép code
+         */
+    }
+}
